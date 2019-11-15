d936819392eef225a845c4a7dcd980c3b1891b0a
Zach Justice <zach.j.justice@gmail.com>
2019-11-14 20:02:12 -0500

    part 2 wip

--- src/aoc_day_16/core.clj
+++ src/aoc_day_16/core.clj
@@ -1,6 +1,7 @@
 (ns aoc-day-16.core
   (:gen-class))
-(def filename "input.txt")
+(def part1-filename "part1.txt")
+(def part2-filename "part2.txt")
 
 (defn mapify
   "take a single sample from the input and convert to a usable map"
@@ -17,31 +18,28 @@
 (defn parse-line
   [line]
   (->> line
-       (re-seq #"\d")
+       (re-seq #"\d+")
        (map read-string)
        (vec)))
+
+(defn listify-string
+  [string]
+  (map parse-line (filter #(not (clojure.string/blank? %1)) (clojure.string/split string #"\n"))))
 
 (defn parse
   "output is list of samples [{:input [r1 r2 r3 r4] :op 'opcode A B C' :ouput [r1 r2 r3 r4]}]"
   [string]
-  (def lines (vec (map parse-line (filter #(not (clojure.string/blank? %1)) (clojure.string/split string #"\n")))))
-  (mapify lines))
-
-(defn get-safe
-  [coll index]
-  (if (< index (count coll))
-    (get coll index)
-    (throw (Exception. "Invalid get"))))
+  (mapify (vec (listify-string string))))
 
 (defn opr
   [op a b c regs]
-  (let [register-a (get-safe regs a)
-        register-b (get-safe regs b)]
+  (let [register-a (get regs a)
+        register-b (get regs b)]
     (assoc regs c (op register-a register-b))))
 
 (defn opi
   [op a b c regs]
-  (let [register-a (get-safe regs a)]
+  (let [register-a (get regs a)]
     (assoc regs c (op register-a b))))
 
 (defn set-op
@@ -58,7 +56,6 @@
   [a b c regs]
   (apply opi [+ a b c regs]))
 
-
 (defn multr
   "(add register) stores into register C the result of adding register A and register B."
   [a b c regs]
@@ -69,7 +66,6 @@
   [a b c regs]
   (apply opi [* a b c regs]))
 
-
 (defn banr
   "(add register) stores into register C the result of adding register A and register B."
   [a b c regs]
@@ -79,7 +75,6 @@
   "(add register) stores into register C the result of adding register A and register B."
   [a b c regs]
   (apply opi [bit-and a b c regs]))
-
 
 (defn borr
   "(add register) stores into register C the result of adding register A and register B."
@@ -102,54 +97,61 @@
   (assoc regs c a))
 
 (defn gtir
-  "(greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0. "
+  "(greater-than immediate/register) sets register C to 1 if value A is greater than register B.
+  Otherwise, register C is set to 0. "
   [a b c regs]
   (apply set-op [> a (get regs b) c regs]))
 
 (defn gtri
-  "(greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0."
+  "(greater-than register/immediate) sets register C to 1 if register A is greater than value B.
+  Otherwise, register C is set to 0."
   [a b c regs]
   (apply set-op [> (get regs a) b c regs]))
 
 (defn gtrr
-  "(greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0."
+  "(greater-than register/register) sets register C to 1 if register A is greater than register B.
+  Otherwise, register C is set to 0."
   [a b c regs]
   (apply set-op [> (get regs a) (get regs b) c regs]))
 
 (defn eqir
-  "(equal-than immediate/register) sets register C to 1 if value A is equal than register B. Otherwise, register C is set to 0. "
+  "(equal-than immediate/register) sets register C to 1 if value A is equal than register B.
+  Otherwise, register C is set to 0. "
   [a b c regs]
   (apply set-op [= a (get regs b) c regs]))
 
 (defn eqri
-  "(equal-than register/immediate) sets register C to 1 if register A is equal than value B. Otherwise, register C is set to 0."
+  "(equal-than register/immediate) sets register C to 1 if register A is equal than value B.
+  Otherwise, register C is set to 0."
   [a b c regs]
   (apply set-op [= (get regs a) b c regs]))
 
 (defn eqrr
-  "(equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0."
+  "(equal register/register) sets register C to 1 if register A is equal to register B.
+  Otherwise, register C is set to 0."
   [a b c regs]
   (apply set-op [= (get regs a) (get regs b) c regs]))
 
 (defn apply-op-code
   [sample op]
-  (println "sample:" sample)
+  ;; (println sample)
   (let [args (:args sample)
         initial-registers (:initial-registers sample)
         expected-registers (:expected-registers sample)]
-    (println op)
+    ;; (println op)
     (try
       (do
         (def actual-registers (apply op (conj args initial-registers)))
-        (println actual-registers)
-        (println expected-registers)
+        ;; (println actual-registers)
+        ;; (println expected-registers)
         (if (= actual-registers expected-registers) 1 0))
       (catch Exception e
         (do 0))
       )))
 
 (defn apply-op-codes
-  "apply each opcode to the sample and determine if the expected register state matches the actual register state"
+  "apply each opcode to the sample and determine if the expected register state matches the actual
+  register state"
   [sample ops]
   (reduce + (map #(apply-op-code sample %1) ops)))
 
@@ -159,11 +161,43 @@
        (map #(if (>= %1 3) 1 0))
        (reduce +)))
 
+(defn part1
+  [file ops]
+  (->> (parse (slurp file))
+       (map #(apply-op-codes %1 ops))
+       (tally-counts)))
+
+(defn construct-command
+  [command-map input]
+  (let [op (get input 0)
+        args (subvec input 1 4)]
+   (hash-map
+    :op (command-map op)
+    :args args)))
+
+(defn run-program
+  [commands]
+  (reduce (fn [registers, sample]
+            (let [{op :op args :args} sample]
+              (apply op (conj args registers))))
+          [0 0 0 0]
+          commands))
+
+(defn part2
+  [part2-filename ops]
+  (->> part2-filename
+       (slurp)
+       (listify-string)                                     ;; produce the ([op ...args] [] [])
+       (map #(construct-command command-map %1))            ;; produce program ({:op :args} {})
+       (run-program)                                        ;; takes the commands and produces the final registers
+       (first)                                              ;; we only care about first register
+       ))
+
 (def ops [addi addr multi multr bani banr borr bori seti setr gtir gtri gtrr eqir eqri eqrr])
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (->> (parse (slurp filename))
-       (map #(apply-op-codes %1 ops))
-       (tally-counts)))
+  (println "part 1 answer: " (part1 part1-filename ops))
+  ;; (println "part 2 answer: " (part2 part2-filename ops))
+  )
 

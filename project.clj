2361f4dd042a10fa373d64ff76695682c230ddc5
zachjustice <zachjustice123@gmail.com>
2019-12-07 01:17:08 -0500

    project.clj

--- project.clj
+++ project.clj
@@ -1,16 +1,9 @@
-(defproject advent "0.1.0-SNAPSHOT"
+(defproject day01 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [digest "1.4.4"]
-                 [medley "0.8.4"]
-                 [criterium "0.4.4"]
-                 [clj-time "0.15.1"]
-                 [net.mikera/core.matrix "0.57.0"]
-                 [org.clojure/math.combinatorics "0.1.1"]
-                 [org.clojure/core.match "0.2.2"]]
-
-  :profiles { :repl { :repl-options {:init (set! *print-length* 500)}} })
-
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot day01.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
